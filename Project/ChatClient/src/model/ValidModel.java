d87b031dc1d7ca4f16671601a00e185563466dc1
Thanh Vuong <thanh952st@gmail.com>
2020-11-08 20:50:19 +0700

    Remake

--- Project/ChatClient/src/model/ValidModel.java
+++
@@ -1,5 +0,0 @@
-package model;
-
-public interface ValidModel {
-	public boolean isValid();
-}
--- Project/ChatQualityAssurance/src/libraryTest/RoomDAO_Test.java
+++
@@ -1,9 +0,0 @@
-package libraryTest;
-
-import data_access.room_access.RoomDAO;
-
-public class RoomDAO_Test {
-	
-	RoomDAO dao;
-	
-}
